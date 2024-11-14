081c2db68f7329db28e08b5f9e576bab83e3776e
ryosukeohara <221109@st.yoshida-g.ac.jp>
2024-11-14 10:33:04 +0900

    まだ決まった場所にしかでないけど一応完成

--- GradProject_O/code/goal_manager.cpp
+++ GradProject_O/code/goal_manager.cpp
@@ -6,11 +6,25 @@
 //==========================================================
 #include "goal_manager.h"
 #include "goal.h"
+#include "camera.h"
+#include "camera_action.h"
+#include "camera_manager.h"
+#include "player.h"
+#include "player_manager.h"
 
 //==========================================================
 // 静的メンバ変数
 //==========================================================
 CGoalManager *CGoalManager::m_pInstance = nullptr;
+
+namespace
+{
+	const D3DXVECTOR3 POS[2] =
+	{
+		D3DXVECTOR3(-8600.0f, 0.0f, -10600.0f),
+		D3DXVECTOR3(0.0f, 0.0f, -4000.0f),
+	};
+}
 
 //==========================================================
 // コンストラクタ
@@ -50,7 +64,7 @@ CGoalManager* CGoalManager::Create(void)
 //==========================================================
 HRESULT CGoalManager::Init(void)
 {
-	//m_pGoal = CGole::Create(D3DXVECTOR3(10000.0f, 0.0f, 12500.0f), 600.0f, 20.0f);
+	m_pGoal = CGole::Create(D3DXVECTOR3(10000.0f, 0.0f, 12500.0f), 600.0f, 20.0f);
 
 	return S_OK;
 }
@@ -79,5 +93,20 @@ void CGoalManager::Uninit(void)
 //==========================================================
 void CGoalManager::Update(void)
 {
+	CCamera* pCamera = CCameraManager::GetInstance()->GetTop();
 
+	CPlayer* pPlayer = CPlayerManager::GetInstance()->GetTop();
+	int nNum = pPlayer->GetNumDeliverStatus();
+
+	if (m_pGoal == nullptr) { return; }
+
+	if (m_pGoal->GetEnd() && pCamera->GetAction()->IsFinish())
+	{
+		m_pGoal->Uninit();
+		m_pGoal = nullptr;
+
+		if (nNum >= 3) { return; }
+
+		m_pGoal = CGole::Create(POS[nNum - 1], 600.0f, 20.0f);
+	}
 }
