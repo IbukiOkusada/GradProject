50d23879cef27ba7e2ed4ade3f38bb28be5fad90
Ibuki Okusada <221038@st.yoshida-g.ac.jp>
2024-11-21 14:19:37 +0900

    検問用向き直した

--- GradProject_O/code/police_manager.cpp
+++ GradProject_O/code/police_manager.cpp
@@ -189,6 +189,8 @@ void CPoliceManager::SetInspection()
 	D3DXVECTOR3 rot = VECTOR3_ZERO;
 	float targetrot = atan2f(pPrev->GetPosition().x - pRoad->GetPosition().x,
 		pPrev->GetPosition().z - pRoad->GetPosition().z);
+	rot.y = targetrot + D3DX_PI * 0.5f;
+	Adjust(rot.y);
 
 	// Ý’u
 	D3DXVECTOR3 pos = pRoad->GetPosition();
