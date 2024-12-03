e0ea84e17700dedc0c917dbea34b684242a1b512
ryosukeohara <221109@st.yoshida-g.ac.jp>
2024-12-03 09:42:12 +0900

    エントリーで画面下にでてるプレイヤーモデルが回転するように変更

--- GradProject_O/code/entry.cpp
+++ GradProject_O/code/entry.cpp
@@ -47,6 +47,7 @@ namespace
 
     const D3DXVECTOR3 CAMERA_POS_V = D3DXVECTOR3(320.3f, -300.0f, 91.6f);
     const float LENGTH = 500.0f;
+    const float ROTATION_Y = 0.005f;
 
     const char* MODEL_PATH = "data\\MODEL\\flyingscooter.x";  // プレイヤーのモデル
 }
@@ -127,8 +128,7 @@ HRESULT CEntry::Init(void)
         pCamera->SetPositionV(D3DXVECTOR3(0.0f, 4000.0f, 0.0f));
     }
 
-    //mgr->GetTop()->SetDraw(false);
-
+    // 複数カメラの生成
     for (int i = 0; i < MAX_PLAYER; i++)
     {
         m_ppCamera[i] = DEBUG_NEW CMultiCamera;
@@ -144,27 +144,12 @@ HRESULT CEntry::Init(void)
         viewport.X = (DWORD)((SCREEN_WIDTH / 4.0f) * (i % WIDTH_NUM));
         viewport.Y = (DWORD)(SCREEN_HEIGHT * 0.7f);
 
-        /*if constexpr (MAX_PLAYER < WIDTH_NUM) {
-            viewport.Width = (DWORD)(SCREEN_WIDTH * 1.0f);
-        }
-        else
-        {
-            viewport.Width = (DWORD)(SCREEN_WIDTH * 0.5f);
-        }
-
-        if constexpr (MAX_PLAYER <= WIDTH_NUM) {
-            viewport.Height = (DWORD)(SCREEN_HEIGHT * 1.0f);
-        }
-        else
-        {
-            viewport.Height = (DWORD)(SCREEN_HEIGHT * 0.5f);
-        }*/
-
         viewport.Width = (DWORD)(SCREEN_WIDTH / 4.0f);
         viewport.Height = (DWORD)(SCREEN_HEIGHT * 0.4f);
 
         viewport.MinZ = 0.0f;
         viewport.MaxZ = 1.0f;
+
         m_ppCamera[i]->SetViewPort(viewport);
     }
   
@@ -192,8 +177,7 @@ void CEntry::Uninit(void)
         m_ppCamera = nullptr;
     }
 
-
-    // カメラの破棄
+    // 画面下にでるプレイヤーモデルの破棄
     if (m_ppObj != nullptr)
     {
         for (int i = 0; i < MAX_PLAYER; i++)
@@ -229,7 +213,7 @@ void CEntry::Update(void)
         if (m_ppObj[i] != nullptr)
         {
             D3DXVECTOR3 rot = m_ppObj[i]->GetRotation();
-            rot.y += 0.005f;
+            rot.y += ROTATION_Y;
             m_ppObj[i]->SetRotation(rot);
         }
     }
@@ -240,24 +224,22 @@ void CEntry::Update(void)
     // プレイヤー参加取り消し処理
     DecreasePlayer();
 
+    // シーンの更新
     CScene::Update();
 
     CPlayerManager* mgr = CPlayerManager::GetInstance();
+
+    // デバック表示
     CDebugProc::GetInstance()->Print("プレイヤーの数[%d]\n", mgr->GetNum());
 
     for (int i = 0; i < MAX_PLAYER; i++)
     {
+        // カメラ
         D3DXVECTOR3 CamPosV = m_ppCamera[i]->GetPositionV();
         D3DXVECTOR3 CamPosR = m_ppCamera[i]->GetPositionR();
         CDebugProc::GetInstance()->Print("カメラ 視点   : [%f, %f, %f]\n", CamPosV.x, CamPosV.y, CamPosV.z);
         CDebugProc::GetInstance()->Print("カメラ 注視点 : [%f, %f, %f]\n", CamPosR.x, CamPosR.y, CamPosR.z);
         CDebugProc::GetInstance()->Print("距離 [%f]\n", m_ppCamera[i]->GetLength());
-
-        if (m_ppObj[i] != nullptr)
-        {
-            D3DXVECTOR3 ObjPosR = m_ppObj[i]->GetPosition();
-            CDebugProc::GetInstance()->Print("プレイヤー   : [%f, %f, %f]\n", ObjPosR.x, ObjPosR.y, ObjPosR.z);
-        }
     }
 }
 
@@ -291,13 +273,20 @@ void CEntry::AddPlayer(void)
             playid = net->GetIdx();
         }
 
+        // プレイヤー生成
         CPlayer* pPlayer = CPlayer::Create(pos, D3DXVECTOR3(0.0f, CAMERA_ROT[id].y, 0.0f), VECTOR3_ZERO, playid);
+
+        // データ受信
         pPlayer->SetType(CPlayer::TYPE::TYPE_RECV);
         pPlayer->SetRecvPosition(pos);
 
+        // チュートリアル時のアクティブに設定
         pPlayer->SetType(CPlayer::TYPE::TYPE_TUTOLERIAL_ACTIVE);
+
+        // エフェクトの終了
         pPlayer->EffectUninit();
 
+        // 画面下にプレイヤーのモデルを表示
         pos = m_ppCamera[id]->GetPositionR();
         m_ppObj[id] = CObjectX::Create(pos, D3DXVECTOR3(0.0f, CAMERA_ROT[id].y, 0.0f), "data\\MODEL\\flyingscooter.x", 7);
         m_ppObj[id]->SetType(CObject::TYPE::TYPE_PLAYER);
@@ -314,19 +303,27 @@ void CEntry::AddPlayer(void)
             // 人数が多い
             if (player == nullptr && net->GetConnect(i))
             {
+                // プレイヤー生成
                 D3DXVECTOR3 pos = m_ppCamera[i]->GetPositionR();
                 CPlayer* pPlayer = CPlayer::Create(pos, D3DXVECTOR3(0.0f, CAMERA_ROT[i].y, 0.0f), VECTOR3_ZERO, i);
+
+                // データ受信
                 pPlayer->SetType(CPlayer::TYPE::TYPE_RECV);
                 pPlayer->SetRecvPosition(pos);
 
                 if (i == net->GetIdx())
                 {
+                    // データ送信
                     pPlayer->SetType(CPlayer::TYPE::TYPE_SEND);
                 }
 
+                // チュートリアル時のアクティブに設定
                 pPlayer->SetType(CPlayer::TYPE::TYPE_TUTOLERIAL_ACTIVE);
+
+                // エフェクト終了
                 pPlayer->EffectUninit();
 
+                // 画面下にプレイヤーのモデルを表示
                 pos = m_ppCamera[i]->GetPositionR();
                 m_ppObj[i] = CObjectX::Create(pos, D3DXVECTOR3(0.0f, CAMERA_ROT[i].y, 0.0f), "data\\MODEL\\flyingscooter.x", 7);
                 m_ppObj[i]->SetType(CObject::TYPE::TYPE_PLAYER);
@@ -340,7 +337,8 @@ void CEntry::AddPlayer(void)
         m_IsFinish = true;
 
         if (m_IsFinish)
-        {
+        {// チュートリアルが終了している
+
             net->SendTutorialOk();
         }
         else
@@ -368,10 +366,12 @@ void CEntry::DecreasePlayer(void)
             // 人数が多い
             if (player != nullptr && !net->GetConnect(i))
             {
+                // プレイヤーの終了処理
                 player->Uninit();
 
                 if (m_ppObj[i] != nullptr)
-                {
+                {// 画面下に出てるプレイヤーモデルの終了
+
                     m_ppObj[i]->Uninit();
                     m_ppObj[i] = nullptr;   
                 }
