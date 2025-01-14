a9e8f0f5297259c5268f26e1354fbf0d05d0f226
kenta <221123@st.yoshida-g.ac.jp>
2025-01-14 10:08:29 +0900

    .cpp、.hを追加

---
+++ GradProject_O/code/robot_manager.h
@@ -0,0 +1,52 @@
+//==========================================================
+//
+// サンプルマネージャー [robot_manager.h]
+// Author : Ibuki Okusada
+//
+//==========================================================
+#ifndef _SAMPLEMANAGER_H_		// このマクロが定義されていない場合
+#define _SAMPLEMANAGER_H_		// 二重インクルード防止用マクロを定義
+
+// 前方宣言
+class CRobot;
+
+#include "list_manager.h"
+
+//==========================================================
+// サンプルマネージャーのクラス定義
+//==========================================================
+class CRobotManager : public CListManager
+{
+private:
+
+	CRobotManager();		// コンストラクタ
+	~CRobotManager();	// デストラクタ
+
+public:	// 誰でもアクセス可能
+
+	// メンバ関数
+	HRESULT Init(void);
+	void Uninit(void);
+	void Update(void);
+	static CRobotManager* GetInstance(void);
+	static void Release(void);
+	CRobot* GetTop(void) { return m_pTop; }
+	CRobot* GetCur(void) { return m_pCur; }
+	void ListIn(CRobot* pSample);
+	void ListOut(CRobot* pSample);
+	int GetNum(void) { return m_nNum; }
+
+private:	// 自分だけがアクセス可能
+
+	// メンバ関数
+
+
+	// メンバ変数
+	CRobot* m_pTop;	// 先頭
+	CRobot* m_pCur;	// 最後尾
+	int m_nNum;
+	static CRobotManager* m_pInstance;	// インスタンス
+};
+
+#endif
+
